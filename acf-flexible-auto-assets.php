245e1654fc31c47b624426f63d23c01c6d368e3e
Viktor Szépe <viktor@szepe.net>
2025-05-20 13:49:49 +0000

    Add plugin header

--- acf-flexible-auto-assets.php
+++ acf-flexible-auto-assets.php
@@ -1,6 +1,23 @@
 <?php
 
-// Me = plugin
+/**
+ * Auto Assets for ACF Flexible Content
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Auto Assets for ACF Flexible Content
+ * Plugin URI:        https://github.com/szepeviktor/acf-flexible-auto-assets
+ * Description:       Automatically enqueue CSS and JavaScript files for ACF Flexible Content blocks
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Requires Plugins:  advanced-custom-fields-pro
+ * Author:            Viktor Szépe
+ * Author URI:        https://github.com/szepeviktor
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+declare(strict_types=1);
 
 use SzepeViktor\WordPress\ACF\FlexibleAssets\AutoAssets;
 
--- src/AutoAssets.php
+++ src/AutoAssets.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SzepeViktor\WordPress\ACF\FlexibleAssets;
 
 use function get_field;
